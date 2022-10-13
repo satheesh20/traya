c7e88d30587b5b3ae22434eaff9a75262499e27a
Satheesh206 <satish.nayaka@edumatica.io>
2022-10-13 15:14:19 +0530

    intial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from "@mui/material";
-import { theme } from "../utils/theme";
-import createEmotionCache from '../utils/createEmotionCache';
-import { CacheProvider, EmotionCache } from '@emotion/react';
+import { theme } from "../../utils/theme";
+import createEmotionCache from '../../utils/createEmotionCache';
+import { CacheProvider } from '@emotion/react';
 const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }: AppProps) {
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,19 @@
+import { Grid } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme)=>({
+    heading: {
+        
+    }
+}))
+function LoginPage() {
+    return ( 
+        <>
+        <Grid sm ={12}>
+            
+        </Grid>
+        </>
+     );
+}
+
+export default LoginPage;
